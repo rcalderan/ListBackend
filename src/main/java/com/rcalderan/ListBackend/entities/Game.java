ffca4cb387df8006cf11ce491e32da9c029f5f80
Richard Calderan <richardcck@hotmail.com>
2025-01-20 12:32:58 -0300

    Seeding table

--- src/main/java/com/rcalderan/ListBackend/entities/Game.java
+++ src/main/java/com/rcalderan/ListBackend/entities/Game.java
@@ -3,20 +3,25 @@ package com.rcalderan.ListBackend.entities;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "game")
+@Table(name = "tb_game")
 public class Game {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String tittle;
+    private String title;
 
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 
@@ -28,12 +33,12 @@ public class Game {
         this.id = id;
     }
 
-    public String getTittle() {
-        return tittle;
+    public String getTitle() {
+        return title;
     }
 
-    public void setTittle(String tittle) {
-        this.tittle = tittle;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public Integer getYear() {
@@ -52,12 +57,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -86,14 +99,17 @@ public class Game {
 
     public Game(){
     }
-    public Game(Long id, String tittle, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String tittle, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
-        this.tittle = tittle;
+        this.title = tittle;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
+
+
 }
