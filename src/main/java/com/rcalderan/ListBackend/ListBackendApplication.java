08ba21075670d1908ed9305c3865c1b68f7c5c32
Richard Calderan <richardcck@hotmail.com>
2025-01-20 10:47:14 -0300

    Project Created

---
+++ src/main/java/com/rcalderan/ListBackend/ListBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rcalderan.ListBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcalderan/ListBackend/ListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcalderan.ListBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
